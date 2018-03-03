887afe276e755aa5a368cbdd6383398695efb978
Michael Li <michael.li1324@gmail.com>
2018-03-03 10:03:52 -0800

    Initial commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../youtube-bot.zip');
+var kuduApi = 'https://youtube-bot.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$youtube-bot';
+var password = 'xtmQmTrpPdeiDZRPBAXthdxYi8b5RnE77jD1zjAEwgKxisKo87Q5h7qrRake';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('youtube-bot publish');
+  } else {
+    console.error('failed to publish youtube-bot', err);
+  }
+});
